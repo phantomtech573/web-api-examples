952f0c9ae45925a91441a471b2c14d14478bc663
MGlenn <phantomtech573@gmail.com>
2026-03-19 10:37:45 -0500

    Update app.js

--- authorization/authorization_code_pkce/public/app.js
+++ authorization/authorization_code_pkce/public/app.js
@@ -8,7 +8,8 @@
  */
 
 const clientId = 'yourClientIDGoesHere'; // your clientId
-const redirectUrl = 'eg:http://localhost:8080';        // your redirect URL - must be localhost URL and/or HTTPS
+const redirectUrl = 'eg:http://localhost:8080';https://www.invest86.cloudflareaccess.com’
+- must be localhost URL and/or HTTPS
 
 const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
